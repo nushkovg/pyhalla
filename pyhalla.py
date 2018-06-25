0e5edc1f51d48401c5f9d85f715dd2c0244d2cae
Goran Nushkov <gnuskov@protonmail.com>
2018-06-25 17:57:45 +0200

    pyhalla.py

--- pyhalla.py
+++ pyhalla.py
@@ -13,22 +13,22 @@ def intCheck():
             userInput = int(input('> '))
             return userInput
         except ValueError:
-            print(f"Do you know what numbers are, {username}? Please type a number if you do.")
+            print("Do you know what numbers are, {}? Please type a number if you do.".format(username))
             return intCheck()
 
 # A function that throws an error message if the user input is invalid.
 def strError(choiceOne, choiceTwo, funcName):
-    print(f"Are you feeling ill, {username}? Decide, " +  choiceOne + " or " +  choiceTwo + ".")
+    print("Are you feeling ill, {}? Decide, ".format(username) +  choiceOne + " or " +  choiceTwo + ".")
     return funcName()
 
 # Exit function that is called when the user chooses to flee from battle. Ends the script.
 def coward():
-    print(f"You call yourself a Viking, {username}!? You are exiled into the unknown.")
+    print("You call yourself a Viking, {}!? You are exiled into the unknown.".format(username))
     exit(1)
 
 # Exit function that is called when the user chooses to stay in the dungeon in Mercia. Ends the script.
 def rot():
-    print(f"You call yourself a Viking, {username}!? You rot away and never reach Valhalla.")
+    print("You call yourself a Viking, {}!? You rot away and never reach Valhalla.".format(username))
     exit(1)
 
 # Simple function that prints several dots between events for readability in the terminal.
@@ -52,16 +52,16 @@ def weaponConditions():
     # Ensures that the user types in a number in range of 1 to 3(4).
     if 1 <= choice < len(weapons):
         chosenWeapon = weapons[choice - 1]
-        print(f"You have chosen the {chosenWeapon}.")
+        print("You have chosen the {}.".format(chosenWeapon))
         dots()
         return chosenWeapon
     elif choice == 4:
         chosenWeapon = weapons[choice - 1]
-        print(f"You have chosen {chosenWeapon}. The gods are pleased with your bright mind, {username}.")
+        print("You have chosen {}. The gods are pleased with your bright mind, {}.".format(chosenWeapon, username))
         dots()
         return chosenWeapon
     else:
-        print(f"Sorry, these are the only ones available, {username}. Choose one of them.")
+        print("Sorry, these are the only ones available, {}. Choose one of them.".format(username))
         return weaponConditions() 
 
 # Asks the user to choose a companion which is stored in a variable, and returned by the function.
@@ -79,11 +79,11 @@ def companionConditions():
 
     if 1 <= choice < (len(companions) + 1):
         chosenComp = companions[choice - 1]
-        print(f"You have chosen {chosenComp} as a companion. Great choice!")
+        print("You have chosen {} as a companion. Great choice!".format(chosenComp))
         dots()
         return chosenComp
     else:
-        print(f"A companion in battle is like wearing a second armour, {username}. Please pick one.")
+        print("A companion in battle is like wearing a second armour, {}. Please pick one.".format(username))
         return companionConditions()
 
 # Start function that starts after a weapon and a companion are chosen.
@@ -91,8 +91,8 @@ def start(comp):
     print("\nYou arrive in Wessex on a boat built by Floki the Builder.")
     print("You are a part of the Great Heathen Army, preparing to raid everything from Wessex to Northumbria.")
     print("You have decided that you are old and it's time to meet the sharp end of the blade and reach Valhalla!")
-    print(f"{comp} will fight along you until the end, so you know a great battle lies ahead!\n")
-    print(f"{comp} sees a road ahead. He asks for your wisdom. Help him choose if he should lead right or left.")
+    print("{} will fight along you until the end, so you know a great battle lies ahead!\n".format(comp))
+    print("{} sees a road ahead. He asks for your wisdom. Help him choose if he should lead right or left.".format(comp))
     
     startConditions()
 
@@ -118,7 +118,7 @@ def startConditions():
 # Function that is called when the user chooses to go left.
 def winchester(wep, comp):
     print("\nYou arrive in a town called Winchester. The Saxons know you are here and have prepared!")
-    print(f"You take out your {wep} and look at {comp}. You both start yelling and dive into battle.")
+    print("You take out your {} and look at {}. You both start yelling and dive into battle.".format(wep, comp))
     print("After a short battle, the Vikings won. But suddenly, three Saxons show up and attack you. What do you do?")
 
     for i in range(1, len(winchesterBattleOptions) + 1):
@@ -148,8 +148,8 @@ def winchesterConditions():
 # Function that is called when the user chooses to go right.
 def abingdon(wep, comp):
     print("\nYou arrive in a town called Abingdon. Their king Ecbert seems to be here too!")
-    print(f"You take out your {wep} and look at {comp}. You both start yelling and dive into battle.")
-    print(f"You see that {comp} is surrounded by King Ecbert himself and his guards! How are you going to help him?")
+    print("You take out your {} and look at {}. You both start yelling and dive into battle.".format(wep, comp))
+    print("You see that {} is surrounded by King Ecbert himself and his guards! How are you going to help him?".format(comp))
 
     for i in range(1, len(abingdonBattleOptions) + 1):
         print(str(i) + ".", end="")
@@ -180,7 +180,7 @@ def mercia(wep, comp):
     print("\nAfter your victory in Wessex, you arrive in Mercia.")
     print("The Great Heathen Army has had a long run and decided to set up a camp. After a long feast, you fell asleep.")
     print("The camp is ambushed by some strong warriors and you are taken as their prisoner.")
-    print(f"They have taken your {wep} and you see {comp} running to help you, but it's all in vain.")
+    print("They have taken your {} and you see {} running to help you, but it's all in vain.".format(wep, comp))
     print("After few nights in the dungeon, you see your chance to escape, as both of the guards are asleep.\n")
     print("You see a shiv on the ground in the dungeon. What is your plan?")
 
@@ -211,8 +211,8 @@ def merciaConditions():
 # Function that is called from mercia() when the user finishes working with it.
 def northumbria(comp):
     print("\nAfter the events in Mercia, you finally arrive in Northumbria, where you meet the filthy King Aelle.")
-    print(f"He surrenders after seeing the might of the Great Heathen Army, and {comp} proposed a Blood Eagle death sentence!\n")
-    print(f"{comp} wants to give you the honor of doing it. Do you accept?")
+    print("He surrenders after seeing the might of the Great Heathen Army, and {} proposed a Blood Eagle death sentence!\n".format(comp))
+    print("{} wants to give you the honor of doing it. Do you accept?".format(comp))
     
     northumbriaConditions()
 
@@ -237,10 +237,10 @@ def northumbriaConditions():
 
 # End function that ends the script successfully
 def end(wep, comp):
-    print(f"\n{comp} gives you his axe and you skinned the god out of Aelle!")
+    print("\n{} gives you his axe and you skinned the god out of Aelle!".format(comp))
     print("But suddenly you feel a knife through your back! You turn around and see a priest shaking with blood on his hands!")
-    print(f"{comp} takes your {wep} and cuts his head off. You fall on the ground and start laughing about the way you are going to Valhalla.")
-    print(f"{comp} and all of the Vikings start yelling your name as they send you to the hall of the mighty gods and heroes.")
+    print("{} takes your {} and cuts his head off. You fall on the ground and start laughing about the way you are going to Valhalla.".format(comp, wep))
+    print("{} and all of the Vikings start yelling your name as they send you to the hall of the mighty gods and heroes.".format(comp))
     print("You die with a smile on your face. You finally met your destiny and reached Valhalla where you are laughing with the gods about the way you died for eternity.\n")
     print("Thanks for playing Pyhalla v1.0 !")
 
